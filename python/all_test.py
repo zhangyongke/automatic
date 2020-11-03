f18d8e94e6ebcaa385980dc93fb02d41e87c635f
zhangyongke <zhangyongke1105@163.com>
2020-11-03 10:49:57 +0800

    变更python3可使用的HTMLTestRunner

--- python/all_test.py
+++ python/all_test.py
@@ -5,25 +5,29 @@
 # @File    : all_test.py
 # @Software : PyCharm
 import unittest, time
-import HTMLTestRunner_PY3
+from HTMLTestRunner import HTMLTestRunner
+
+
 def createsuite():
     testunit = unittest.TestSuite()
-    ##定义测试文件查找的目录
+    # 定义测试文件查找的目录
     test_dir = u'XXX路径'
-    ##定义discover方法
+    # 定义discover方法
     discover = unittest.defaultTestLoader.discover(test_dir,pattern='test*.py',top_level_dir=None)
     for test_suite in discover:
         for test_case in test_suite:
             testunit.addTests(test_case)
     return testunit
+
+
 now = time.strftime("%Y-%m-$d %H_%M_%S")
 
-##定义报告文件存放路径
-filename = '路径地址' + now + 'result.html'    ##如'E:\\workplace\\python\\result\\'
+# 定义报告文件存放路径
+filename = '路径地址' + now + 'result.html'    # 如'E:\\workplace\\python\\result\\'
 fp = open(filename, 'wb')
-runner = HTMLTestRunner_PY3.HTMLTestRunner(fp, verbosity=1, title='XXX项目测试报告', description=u'用例执行情况')
+runner = HTMLTestRunner.HTMLTestRunner(fp, verbosity=1, title='XXX项目测试报告', description=u'用例执行情况')
 
-if __name__=='__main__':
+if __name__ == '__main__':
     alltestnames = createsuite()
     runner.run(alltestnames)
     fp.close()
