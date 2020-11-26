6e11795c5eece6ef7949e51d1cc3fc01f534c975
zhangyongke <zhangyongke1105@163.com>
2020-11-26 19:26:34 +0800

    1、格式调整

--- python/test.py
+++ python/test.py
@@ -6,126 +6,126 @@
 # @Software : PyCharm
 
 
-# 在列表之间移动元素
-# 首先，创建一个待验证码用户列表，和一个用于存储已验证用户的空列表
-unconfirmed_users = ['alic', 'brian', 'candace']
-confirmed_users = []
-# 验证每个用户，直到没有未验证的用户为止，并将每个验证过的用户都添加到已验证用户列表中
-while unconfirmed_users:
-    current_user = unconfirmed_users.pop()
-    print("Verifying user: " + current_user.title())
-    confirmed_users.append(current_user)
-
-# 显示所有已验证过的用户
-print("\nThe following users have been confirmed:")
-for confirmed_user in confirmed_users:
-    print(confirmed_user.title())
-
-
-# 函数关键字实参
-def describe_pet(animal_type, pet_name):
-    """显示宠物信息"""
-    print("\nI have a " + animal_type + ".")
-    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
-
-
-describe_pet(animal_type='hamster', pet_name='hungry')
-
-
-# 函数默认值
-def describe_pet(pet_name, animal_type='dog'):
-    """显示宠物信息"""
-    print("\nI have a " + animal_type + ".")
-    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
-
-
-describe_pet(pet_name='willie')
-describe_pet('willie')
-
-
-# 函数返回值
-# def get_format_name(first_name, last_name):
-#     """返回完整的姓名"""
-#     full_name = first_name + ' ' + last_name
+# # 在列表之间移动元素
+# # 首先，创建一个待验证码用户列表，和一个用于存储已验证用户的空列表
+# unconfirmed_users = ['alic', 'brian', 'candace']
+# confirmed_users = []
+# # 验证每个用户，直到没有未验证的用户为止，并将每个验证过的用户都添加到已验证用户列表中
+# while unconfirmed_users:
+#     current_user = unconfirmed_users.pop()
+#     print("Verifying user: " + current_user.title())
+#     confirmed_users.append(current_user)
+#
+# # 显示所有已验证过的用户
+# print("\nThe following users have been confirmed:")
+# for confirmed_user in confirmed_users:
+#     print(confirmed_user.title())
+#
+#
+# # 函数关键字实参
+# def describe_pet(animal_type, pet_name):
+#     """显示宠物信息"""
+#     print("\nI have a " + animal_type + ".")
+#     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+#
+#
+# describe_pet(animal_type='hamster', pet_name='hungry')
+#
+#
+# # 函数默认值
+# def describe_pet(pet_name, animal_type='dog'):
+#     """显示宠物信息"""
+#     print("\nI have a " + animal_type + ".")
+#     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+#
+#
+# describe_pet(pet_name='willie')
+# describe_pet('willie')
+#
+#
+# # 函数返回值
+# # def get_format_name(first_name, last_name):
+# #     """返回完整的姓名"""
+# #     full_name = first_name + ' ' + last_name
+# #     return full_name.title()
+# #
+# #
+# # musician = get_format_name('jimi', 'hendrix')
+# # print(musician)
+#
+#
+# # 让实参变成可选的
+# def get_format_name(first_name, last_name, middle_name=''):
+#     """返回整个的姓名"""
+#     if middle_name:
+#         full_name = first_name + ' ' + middle_name + ' ' + last_name
+#     else:
+#         full_name = first_name + ' ' + last_name
 #     return full_name.title()
 #
 #
 # musician = get_format_name('jimi', 'hendrix')
 # print(musician)
-
-
-# 让实参变成可选的
-def get_format_name(first_name, last_name, middle_name=''):
-    """返回整个的姓名"""
-    if middle_name:
-        full_name = first_name + ' ' + middle_name + ' ' + last_name
-    else:
-        full_name = first_name + ' ' + last_name
-    return full_name.title()
-
-
-musician = get_format_name('jimi', 'hendrix')
-print(musician)
-musician = get_format_name('john', 'hooker', 'lee')
-print(musician)
-
-
-# 返回字典
-def build_person(first_name, last_name, age=''):
-    """返回一个字典，其中包含一个人的信息"""
-    person = {'first': first_name, 'last': last_name}
-    if age:
-        person['age'] = age
-    return person
-
-
-musician = build_person('jimi', 'hendrix', age='17')
-print(musician)
-
-
-# 结合使用函数和while循环
-def get_format_name(first_name, last_name, middle_name=''):
-    """返回整个的姓名"""
-    if middle_name:
-        full_name = first_name + ' ' + middle_name + ' ' + last_name
-    else:
-        full_name = first_name + ' ' + last_name
-    return full_name.title()
-
-
-# 这是一个循环
-# 向函数传递列表
-def greet_users(names):
-    """向列表中的每位用户都发出简单的问候"""
-    for name in names:
-        msg = "Hello," + name.title() + "!"
-        print(msg)
-
-
-usernames = ['hannah', 'try', 'margot']
-greet_users(usernames)
-
-
-# 传递任意数量的实参
-def make_pizza(*toppings):   # 形参名*toppings中的星号让python创建一个名为toppings的空元组，并将收到的所有值都封装到这个元组中
-    """概述要制作的披萨"""
-    print("\nMake a pizza with the following toppings:")
-    for topping in toppings:
-        print("- " + topping)
-
-
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
-
-
-# 使用任意数量的关键字实参
-def build_profile(first, last, **user_info):       # 形参**user_info中的两个星号让python创建一个名为user_info的空字典，并将收到的所有名称-值对都封装到这个字典中
-    """创建一个字典，其中包含我们知道的有关用户的一切"""
-    profile = {'first_name': first, 'last_name': last}
-    for key, value in user_info.items():
-        profile[key] = value
-    return profile
-
-
-user_profile = build_profile('albert', 'einstein', location='princeton', field='physics')
-print(user_profile)
+# musician = get_format_name('john', 'hooker', 'lee')
+# print(musician)
+#
+#
+# # 返回字典
+# def build_person(first_name, last_name, age=''):
+#     """返回一个字典，其中包含一个人的信息"""
+#     person = {'first': first_name, 'last': last_name}
+#     if age:
+#         person['age'] = age
+#     return person
+#
+#
+# musician = build_person('jimi', 'hendrix', age='17')
+# print(musician)
+#
+#
+# # 结合使用函数和while循环
+# def get_format_name(first_name, last_name, middle_name=''):
+#     """返回整个的姓名"""
+#     if middle_name:
+#         full_name = first_name + ' ' + middle_name + ' ' + last_name
+#     else:
+#         full_name = first_name + ' ' + last_name
+#     return full_name.title()
+#
+#
+# # 这是一个循环
+# # 向函数传递列表
+# def greet_users(names):
+#     """向列表中的每位用户都发出简单的问候"""
+#     for name in names:
+#         msg = "Hello," + name.title() + "!"
+#         print(msg)
+#
+#
+# usernames = ['hannah', 'try', 'margot']
+# greet_users(usernames)
+#
+#
+# # 传递任意数量的实参
+# def make_pizza(*toppings):   # 形参名*toppings中的星号让python创建一个名为toppings的空元组，并将收到的所有值都封装到这个元组中
+#     """概述要制作的披萨"""
+#     print("\nMake a pizza with the following toppings:")
+#     for topping in toppings:
+#         print("- " + topping)
+#
+#
+# make_pizza('pepperoni')
+# make_pizza('mushrooms', 'green peppers', 'extra cheese')
+#
+#
+# # 使用任意数量的关键字实参
+# def build_profile(first, last, **user_info):       # 形参**user_info中的两个星号让python创建一个名为user_info的空字典，并将收到的所有名称-值对都封装到这个字典中
+#     """创建一个字典，其中包含我们知道的有关用户的一切"""
+#     profile = {'first_name': first, 'last_name': last}
+#     for key, value in user_info.items():
+#         profile[key] = value
+#     return profile
+#
+#
+# user_profile = build_profile('albert', 'einstein', location='princeton', field='physics')
+# print(user_profile)
---
+++ python/字符串操作.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/11/26 17:16
+# @Author : zyk
+# @Email : zhangyongke1105@163.com
+# @File : 字符串操作.py
+# @Software: PyCharm
+
+# 字符串方法
+# 合并序列  join是一个非常重要的字符串方法，其作用与split相反，用于合并序列的元素
+sep = '+'
+seq = ['1', '2', '3', '4', '5']
+a = sep.join(seq)
+print(a)
+
+dirs = '', 'usr', 'bin', 'env'
+b = '/'.join(dirs)
+print(b)
+
+# 返回字符串的小写版本   lower()
+a = 'Trondheim Hammer Dance'
+b = a.lower()
+print(b)
+
+# 返回字符串首字母大写  title()方法
+a = 'this is an apple'
+b = a.title()
+print(b)
+
+# 将指定子串都替换为另一个字符串，并返回替换后的结果 replace()方法
+a = 'This is a test'
+b = a.replace('is', 'eez')
+print(b)
+
+# 将字符串拆分为序列 split()方法
+a = '1+2+3+4+5'
+b = a.split('+')
+print(b)
+
+a = '/usr/bin/env'
+b = a.split('/')
+print(b)
+
+a = 'Using the default'
+b = a.split()
+print(b)
+
+# 将字符串开头和末尾的空白(不包括中间的空白)删除，并返回删除后的结果 strip()
+a = ' internal whitespace is kept '
+b = a.strip()
+print(b)
+
+# 单字符替换，translate()，然而使用translate前必须创建一个转换表，
+table = str.maketrans('cs', 'kz')
+a = 'this is an incredible test'
+b = a.translate(table)
+print(b)
