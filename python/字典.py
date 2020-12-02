fa025d29461a7fe79e14d4157e9519b38299f39b
zhangyongke <zhangyongke1105@163.com>
2020-12-02 14:07:56 +0800

    1、格式调整

---
+++ python/字典.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/11/27 16:26
+# @Author : zyk
+# @Email : zhangyongke1105@163.com
+# @File : 字典.py
+# @Software: PyCharm
+
+
+# 一个将人名用作键的字典。每个人都用一个字典表示
+people = {
+    'Alice': {
+        'phone': '22341',
+        'addr': "Foo drive 23"
+    },
+    'Beth': {
+        'phone': '9102',
+        'addr': 'Bar street 42'
+    },
+    'Cecil': {
+        'phone': '3158',
+        'addr': 'Baz avenue 90'
+    }
+}
+
+# 电话号码和地址的描述性标签，供打印输出时使用
+labels = {
+    'phone': 'phone number',
+    'addr': 'address'
+}
+
+name = input('Name: ')
+
+# 要查找电话号码还是地址
+request = input('Phone number(p) or address(a)?')
+# 使用正确的键：
+if request == 'p':
+    key = 'phone'
+if request == 'a':
+    key = 'addr'
+# 仅当名字是字典的时候
+if name in people:
+    print("{}'s {} is {}." .format(name, labels[key], people[name][key]))
