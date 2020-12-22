e816149307eea4c82958a7f1e784a333eaca7201
zhangyongke <zhangyongke1105@163.com>
2020-12-22 18:27:31 +0800

    1、项目调整

---
+++ python_project/test_case/test_business_white_list.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+# @Time : 2020/12/22 18:18
+# @Author : zyk
+# @Email : zhangyongke1105@163.com
+# @File : test_business_white_list.py
+# @Software: PyCharm
+
+
+class TestBusinessWhiteList:
+    """测试企业白名单模块的各个功能"""
+
